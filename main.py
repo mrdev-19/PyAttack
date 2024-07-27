9feb2b0b3967694f243bdec37986d6be129a0e72
Chanakya Devendra <chanakyadevendra2004@gmail.com>
2024-07-28 00:09:12 +0530

    Added Initial Interface and Commands

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 version="1.0"
-title='''
+title=f"""
 ooooooooo.                     .o.           .       .                       oooo        
 `888   `Y88.                  .888.        .o8     .o8                       `888        
  888   .d88' oooo    ooo     .8"888.     .o888oo .o888oo  .oooo.    .ooooo.   888  oooo  
@@ -10,6 +10,27 @@ o888o            .8'     o88o     o8888o   "888"   "888" `Y888""8o `Y8bod8P' o88
              .o..P'                                                                      
              `Y8P'                                                                       
 Python Ddos Script
-Made with ❤️ by Dev
-Github: 
+Made with ❤️  by Dev
+Github: https://github.com/mrdev-19/PyAttack
+version : {version}
+"""
+
+from argparse import ArgumentParser,RawTextHelpFormatter
+import requests,colorama
+from termcolor import colored,cprint
+
+def main():
+    parser=ArgumentParser(
+        usage='./(prog)s -t [target] -p [port] -t [threads]',
+        formatter_class=RawTextHelpFormatter,
+        prog='pyattack',
+        description=cprint(title,'cyan',attrs=['bold']),
+        epilog='''
+    ./%(prog)s -d www.example.com -p 80 -T 2000 -Pyslow
+    ./%(prog)s -d www.domain.com -s 100 -Request
+    ./%(prog)s -d www.google.com -Synflood -T 5000 -t 10.0
 '''
+    )
+
+if __name__=="__main__":
+    main()
