d17a21c35b60797941b070cc8a04a8590609f8e7
Chanakya Devendra <chanakyadevendra2004@gmail.com>
2024-07-27 23:51:13 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,15 @@
+version="1.0"
+title='''
+ooooooooo.                     .o.           .       .                       oooo        
+`888   `Y88.                  .888.        .o8     .o8                       `888        
+ 888   .d88' oooo    ooo     .8"888.     .o888oo .o888oo  .oooo.    .ooooo.   888  oooo  
+ 888ooo88P'   `88.  .8'     .8' `888.      888     888   `P  )88b  d88' `"Y8  888 .8P'   
+ 888           `88..8'     .88ooo8888.     888     888    .oP"888  888        888888.    
+ 888            `888'     .8'     `888.    888 .   888 . d8(  888  888   .o8  888 `88b.  
+o888o            .8'     o88o     o8888o   "888"   "888" `Y888""8o `Y8bod8P' o888o o888o 
+             .o..P'                                                                      
+             `Y8P'                                                                       
+Python Ddos Script
+Made with ❤️ by Dev
+Github: 
+'''
